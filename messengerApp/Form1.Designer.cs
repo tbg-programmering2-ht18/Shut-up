84d6d5ff47062c52c161762cf9f54e84e93d6300
Elliott Lie <Elliott.DTK.Lie@lystkom.se>
2019-02-15 12:39:41 +0100

    Add project files.

---
+++ messengerApp/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace messengerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServerIPtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ServerPORTtextBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.ClientPORTtextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ClientIPtextBox = new System.Windows.Forms.TextBox();
+            this.MessagetextBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.ChatScreentextBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // ServerIPtextBox
+            // 
+            this.ServerIPtextBox.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ServerIPtextBox.Location = new System.Drawing.Point(93, 77);
+            this.ServerIPtextBox.Name = "ServerIPtextBox";
+            this.ServerIPtextBox.Size = new System.Drawing.Size(221, 26);
+            this.ServerIPtextBox.TabIndex = 0;
+            this.ServerIPtextBox.TextChanged += new System.EventHandler(this.ServerIPtextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(366, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PORT";
+            // 
+            // ServerPORTtextBox
+            // 
+            this.ServerPORTtextBox.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ServerPORTtextBox.Location = new System.Drawing.Point(431, 77);
+            this.ServerPORTtextBox.Name = "ServerPORTtextBox";
+            this.ServerPORTtextBox.Size = new System.Drawing.Size(221, 26);
+            this.ServerPORTtextBox.TabIndex = 4;
+            this.ServerPORTtextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.Orange;
+            this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(673, 69);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(107, 43);
+            this.StartButton.TabIndex = 5;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Server";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 29);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Client";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.BackColor = System.Drawing.Color.Orange;
+            this.ConnectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ConnectButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectButton.Location = new System.Drawing.Point(673, 196);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(107, 43);
+            this.ConnectButton.TabIndex = 11;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = false;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // ClientPORTtextBox
+            // 
+            this.ClientPORTtextBox.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientPORTtextBox.Location = new System.Drawing.Point(431, 204);
+            this.ClientPORTtextBox.Name = "ClientPORTtextBox";
+            this.ClientPORTtextBox.Size = new System.Drawing.Size(221, 26);
+            this.ClientPORTtextBox.TabIndex = 10;
+            this.ClientPORTtextBox.TextChanged += new System.EventHandler(this.ClientPORTtextBox_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(366, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "PORT";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(25, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 26);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "IP";
+            // 
+            // ClientIPtextBox
+            // 
+            this.ClientIPtextBox.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientIPtextBox.Location = new System.Drawing.Point(93, 204);
+            this.ClientIPtextBox.Name = "ClientIPtextBox";
+            this.ClientIPtextBox.Size = new System.Drawing.Size(221, 26);
+            this.ClientIPtextBox.TabIndex = 7;
+            this.ClientIPtextBox.TextChanged += new System.EventHandler(this.ClientIPtextBox_TextChanged);
+            // 
+            // MessagetextBox
+            // 
+            this.MessagetextBox.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.MessagetextBox.Location = new System.Drawing.Point(93, 455);
+            this.MessagetextBox.Name = "MessagetextBox";
+            this.MessagetextBox.Size = new System.Drawing.Size(559, 26);
+            this.MessagetextBox.TabIndex = 13;
+            this.MessagetextBox.TextChanged += new System.EventHandler(this.MessagetextBox_TextChanged);
+            // 
+            // SendButton
+            // 
+            this.SendButton.BackColor = System.Drawing.Color.Orange;
+            this.SendButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SendButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendButton.Location = new System.Drawing.Point(673, 445);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(107, 43);
+            this.SendButton.TabIndex = 14;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = false;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            this.SendButton.Enter += new System.EventHandler(this.SendButton_Click);
+            // 
+            // ChatScreentextBox
+            // 
+            this.ChatScreentextBox.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ChatScreentextBox.Location = new System.Drawing.Point(93, 253);
+            this.ChatScreentextBox.Multiline = true;
+            this.ChatScreentextBox.Name = "ChatScreentextBox";
+            this.ChatScreentextBox.Size = new System.Drawing.Size(559, 184);
+            this.ChatScreentextBox.TabIndex = 15;
+            this.ChatScreentextBox.TextChanged += new System.EventHandler(this.ChatScreentextBox_TextChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(983, 544);
+            this.Controls.Add(this.ChatScreentextBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.MessagetextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.ClientPORTtextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ClientIPtextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.ServerPORTtextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ServerIPtextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ServerIPtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ServerPORTtextBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox ClientPORTtextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox ClientIPtextBox;
+        private System.Windows.Forms.TextBox MessagetextBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.TextBox ChatScreentextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ messengerApp/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace messengerApp
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client; // listen for connection from TCP network
+        public StreamWriter STW;
+        public StreamReader STR;
+        public string recieve;
+        public string textToSend;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //The name localhost normally resolves to the IPv4 loopback address 127.0.0.1, and to the IPv6 loopback address ::1
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress address in localIP)
+            {
+                //Checks for ipv4
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ServerIPtextBox.Text = address.ToString();
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Check with ex Wireshark 
+                TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(ServerPORTtextBox.Text));
+                listener.Start();
+                this.BackColor = Color.Blue;
+                this.Update();
+                ChatScreentextBox.AppendText("Server started" + "\n");
+                ChatScreentextBox.Update();
+                client = listener.AcceptTcpClient(); //Accept a pending connection request 
+                STR = new StreamReader(client.GetStream());
+                STW = new StreamWriter(client.GetStream());
+                STW.AutoFlush = true;
+                backgroundWorker1.RunWorkerAsync();
+                backgroundWorker2.WorkerSupportsCancellation = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    recieve = STR.ReadLine();
+                    this.ChatScreentextBox.Invoke(new MethodInvoker(delegate ()
+                    {
+                        ChatScreentextBox.AppendText("Other: " + recieve + "\n");
+                    }));
+                    recieve = "";
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(textToSend);
+                this.ChatScreentextBox.Invoke(new MethodInvoker(delegate()
+                {
+                    ChatScreentextBox.AppendText("Me: "+ textToSend + "\n");
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Sending failed");
+            }
+            backgroundWorker2.CancelAsync();
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            if (MessagetextBox.Text !="")
+            {
+                textToSend = MessagetextBox.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            MessagetextBox.Text = "";
+
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+                try
+                {
+                    client = new TcpClient();
+
+                    //Represents a network endpoint as an IP address and a port number.
+                    IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse(ClientIPtextBox.Text), int.Parse(ClientPORTtextBox.Text));
+
+                    //Connects the client to a remote TCP host using the specified host name and port number.
+                    client.Connect(ipEnd);
+                    if (client.Connected)
+                    {
+                        this.BackColor = Color.Green;
+                        this.Update();
+                        ChatScreentextBox.AppendText("Connected to Server" + "\n");
+                        ChatScreentextBox.Update();
+                        STR = new StreamReader(client.GetStream());
+                        STW = new StreamWriter(client.GetStream());
+                        STW.AutoFlush = true;
+                        backgroundWorker1.RunWorkerAsync();
+                        backgroundWorker2.WorkerSupportsCancellation = true;
+                    }
+                }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChatScreentextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MessagetextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClientPORTtextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ServerIPtextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClientIPtextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
